448c23c644c04417731b15f2088aaa2cb4a08d49
Sean Michaletz <seanmichaletz@users.noreply.github.com>
2026-06-08 18:44:02 -0700

    Update and rename Figure_S5.R to Figure_S5_Table_S2.R

--- Figure_S5_Table_S2.R
+++ Figure_S5_Table_S2.R
@@ -1,6 +1,8 @@
 # Boxplots of aboveground biomass per species-treatment combination (Fig. S5)
+# and median aboveground biomass-to-volume ratios (Table S2)
 # for Malamud et al. Nitrogen trait convergence study
 # Sean Michaletz (sean.michaletz@ubc.ca), 13 May 2026
+# Revised 8 June 2026
 
 # 0. Initialize ----
 
@@ -55,3 +57,33 @@ ggsave("Figure_S5.jpg", p_S5,
 library(svglite)
 ggsave("Figure_S5.svg", p_S5,
        width = 8.5, height = 3.75, bg = "white")
+
+# 3. Median biomass-to-volume ratios (Table S2) ----
+
+pot_volume_L <- 0.7
+df$bvr_g_per_L <- df$dry_whole_g / pot_volume_L
+
+med_biomass <- aggregate(dry_whole_g ~ species + treatment_mmol, data = df,
+                         FUN = function(x) round(median(x, na.rm = TRUE), 2))
+med_bvr     <- aggregate(bvr_g_per_L ~ species + treatment_mmol, data = df,
+                         FUN = function(x) round(median(x, na.rm = TRUE), 2))
+n_tab       <- aggregate(dry_whole_g ~ species + treatment_mmol, data = df,
+                         FUN = length)
+
+bvr_summary <- merge(med_biomass, med_bvr, by = c("species", "treatment_mmol"))
+bvr_summary <- merge(bvr_summary, n_tab,   by = c("species", "treatment_mmol"))
+names(bvr_summary) <- c("species", "treatment_mmol",
+                        "median_biomass_g", "median_bvr_g_per_L", "n")
+bvr_summary <- bvr_summary[order(bvr_summary$species, bvr_summary$treatment_mmol), ]
+print(bvr_summary)
+
+# Wide matrix for the table; NA when a species-treatment combination is 
+# absent (i.e. H. vulgare at 0 mM)
+bvr_wide <- tapply(df$bvr_g_per_L,
+                   list(treatment_mmol = df$treatment_mmol, species = df$species),
+                   function(x) round(median(x, na.rm = TRUE), 2))
+# reorder columns to manuscript species order
+sp_order <- c("B. officinalis", "H. vulgare", "R. sativus")
+bvr_wide <- bvr_wide[, sp_order[sp_order %in% colnames(bvr_wide)]]
+print(bvr_wide)
+
